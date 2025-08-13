3a3623871379c29e2aa703039138408ff8e03443
bahrom04 <116780481+bahrom04@users.noreply.github.com>
2025-08-13 18:38:38 +0500

    add 48 gnome runtime (#20)
    * chore: bahrom04 branch for testing
    
    * chore: fuck rust to be honest what the fuck just i did
    
    * chore: added sidebar but not finished ui
    
    * chore: remove unused imports
    
    * chore: add expadable height and width to side bard and content parent
    
    * chore: add rust toolchain snippets
    
    * chore: welcome page initial commit
    
    * chore: add welcome page
    
    * chore: complete merge main to bahrom04 branch
    
    * chore: stack page for welcome and main page
    
    * chore: added certificate struct for dynamic dislay
    
    * chore: remove transition from stack sidebar
    
    * feat: add a function that checks the E-imzo service running in background. Change logo and text in welcome page
    
    * chore: moved function to another folder
    
    * chore: testing select mode widget
    
    * chore: mvp for selection local and usb mode
    
    * chore: add match.page page changer
    
    * chore: mvp verison of open file mode
    
    * CHORE: set placeholder file names
    
    * chore: mvp of finished functionality app
    
    * chore: mvp of finished functionality app
    
    * chore: add interval that refreshes widget if some .pfx files removed or added to /media/DSKEYS
    
    * chore: remove un used codes
    
    * chore: test pkexec
    
    * fix: changed logo and text size
    
    * Delete xinux_manager.png
    
    * fix: changed logo  and text size"
    
    * okl
    
    * chore: fixed
    
    * chore: still checking how to ask user password
    
    * chore: working on fixing systemctl detection
    
    * chore: update meson configs
    
    * chore: user sking password mvp
    
    * feat: add new func
    
    * chore: add aasking password to select_mode fie
    
    * chore: finished mvp version asking password to create /media/DSKEYS
    
    * chore: fix shortcuts menubar
    
    * Merge branch 'main' into bahrom04
    
    * chore: update cargo lock
    
    * chore: remove unused file
    
    * chore: working on awesome e-imzo list dialog and alert dialog when file already exists
    
    * chore: show alert dialog when file already exists in /media/DSKEYS folder
    
    * feat: added a function that checks if a folder exists and permissions
    
    * chore: added awesome list of used e-imzo service websites
    
    * merge bemeritus feature to bahrom04 branch
    
    * chore: remove checking folder permission because it automaticly checks and sets to current user if its root
    
    * chore: set application icon and added "+" button to load certificates
    
    * chore: add uz lang
    
    * chore: update docs
    
    * chore: added gettex to texts
    
    * chore: update uzbek language
    
    * chore: add ja, ru, tajik languages
    
    * fix: uzbek translations
    
    * chore: deleted e-imzo folder and moved to utils.rs
    
    * chore: move i18n and some gtk helper functions
    
    * chore: rename tepmlate name to e-imzo
    
    * CHORE: removed japanese and tajik
    
    * chore: added russian translations
    
    * chore: working on  meson some issues
    
    * chore: add back tracing
    
    * chore: change window width
    
    * chore: changed awesome list to button
    
    * chore: add project name
    
    * chore: update project name
    
    * chore: update project name
    
    * chore: update project name
    
    * chore: flatpak setup initial
    
    * chore: flatpak setup second initial not works
    
    * chore: add production file generator
    
    * chore: add test release action
    
    * chore: add test release action
    
    * chore: add test release action 2
    
    * chore: add test release action final
    
    * chore: add test release action final
    
    * chore: update docs
    
    * chore: update metainfo and translations
    
    * chore: prepare app for production and add flatpak-spawn for only flatpak compability without nix buld support
    
    * chore: rename org.xinux to uz.xinux
    
    * chore: rename org.xinux to uz.xinux
    
    * chore: add 48 runtime and remove network acxcess
    
    ---------
    
    Co-authored-by: bemeritus <bemerituss@gmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -8,9 +8,7 @@ use relm4::{
 use std::{fs, io, os::unix::fs::MetadataExt, path::Path, process::Command};
 
 pub fn is_service_active(service_name: &str) -> Result<bool, String> {
-    let output = Command::new("flatpak-spawn")
-        .arg("--host")
-        .arg("systemctl")
+    let output = Command::new("systemctl")
         .args(&["--user", "is-active", service_name])
         .output()
         .map_err(|e| format!("Failed to run systemctl: {}", e))?;
