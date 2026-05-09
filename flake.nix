f2e0b0a2c6ebdca995a24b5869153c8d8d8973bd
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-09 19:55:00 +0500

    chore: update lock

--- flake.nix
+++ flake.nix
@@ -2,11 +2,7 @@
   description = "A beginning of an awesome project bootstrapped with github:bleur-org/templates";
 
   inputs = {
-    # Stable for keeping thins clean
-    # # Fresh and new for testing
     nixpkgs.url = "git+https://git.oss.uzinfocom.uz/xinux/nixpkgs?ref=nixos-unstable&shallow=1";
-
-    crane.url = "github:ipetkov/crane";
 
     xinux-lib = {
       url = "git+https://git.oss.uzinfocom.uz/xinux/lib?ref=main&shallow=1";
