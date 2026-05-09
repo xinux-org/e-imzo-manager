3491e38cc13096b848292ed21f0f123585ccd9af
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-09 23:29:27 +0500

    chore: change asset path

--- src/ui/welcome.rs
+++ src/ui/welcome.rs
@@ -71,7 +71,7 @@ impl SimpleComponent for WelcomeModel {
 }
 
 fn embedded_logo() -> Texture {
-    let bytes = include_bytes!("../../.github/assets/logo.png");
+    let bytes = include_bytes!("../../.forgejo/assets/logo.png");
     let g_bytes = glib::Bytes::from(&bytes.to_vec());
     let stream = MemoryInputStream::from_bytes(&g_bytes);
     let pixbuf = Pixbuf::from_stream(&stream, Cancellable::NONE).unwrap();
