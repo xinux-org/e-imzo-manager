87fd6439416f5aff6627cf506d559310b85d462b
bahrom04 <magdiyevbahrom@gmail.com>
2026-07-14 14:04:11 +0500

    chore(button): add static variant also

--- src/utils.rs
+++ src/utils.rs
@@ -24,7 +24,7 @@ pub fn is_service_installed() -> bool {
         None => return false,
     };
 
-    matches!(status.as_str(), "enabled")
+    matches!(status.as_str(), "enabled" | "static")
 }
 
 pub fn is_service_active() -> bool {
